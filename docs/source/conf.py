a1e03d252e8ae28826f539f7c1199e52bfc4f8d3
Jeroen Op de Beek <59441635+Jeroenodb@users.noreply.github.com>
2023-01-07 09:43:11 +0100

    added logo, restructured docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,3 +26,8 @@ exclude_patterns = []
 #html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_logo = "images/logo_small.png"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
